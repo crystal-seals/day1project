35734ef2ebff5e6d5c7c2e4febffc9acb892a26f
Richard Warren <rwarren92@hotmail.com>
2016-06-06 16:09:07 +0100

    begin javascript

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// Modal window popup for diseases//
-var modal = document.getElementById('diseaseModal');
-
-var btn = document.getElementById("diseaseButton");
-
-var span = document.getElementsByClassName("closeDisease")[0];
-
-btn.onclick = function() {
-    modal.style.display = "block";
+function checkDisease() {
+  
 }
-
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-//
